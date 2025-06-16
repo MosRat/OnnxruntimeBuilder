076c6f784ae2b608098ccd5f25a5d582ee7d44e4
Hongli Wen <work@whl.moe>
2025-06-16 20:39:17 +0800

    Update build-onnxruntime-win.ps1

--- build-onnxruntime-win.ps1
+++ build-onnxruntime-win.ps1
@@ -53,7 +53,7 @@ param (
 
     [Parameter(Mandatory = $false)]
     [ValidateSet('Release', 'Debug', 'MinSizeRel', 'RelWithDebInfo')]
-    [string] $BuildType = ' RelWithDebInfo'
+    [string] $BuildType = 'RelWithDebInfo'
 )
 
 
